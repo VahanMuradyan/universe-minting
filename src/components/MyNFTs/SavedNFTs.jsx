bdd292eb309dc699419d8a14e596acdf728ec4f2
VahanMuradyan <10vahan.m16@gmail.com>
2021-03-23 18:13:56 +0400

    My NFTs page

---
+++ src/components/MyNFTs/SavedNFTs.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SavedNFTs = () => {
+    return (
+        <div className='tab__saved__nfts'>
+            SavedNFTs
+        </div>
+    )
+}
+
+export default SavedNFTs;
