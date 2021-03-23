a6c13796a9f193ed331a71ec49bf7136a1120ebe
VahanMuradyan <10vahan.m16@gmail.com>
2021-03-23 15:34:03 +0400

    Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,8 +1,18 @@
 import './header.scss';
+import appLogo from '../../assets/images/app-logo.png';
+import Nav from './Nav';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <div className="header">Header</div>
+        <header>
+            <div className='app__logo'>
+                <Link to={'/'}><img src={appLogo} alt='App Logo' /></Link>
+            </div>
+            <div className='nav'>
+                <Nav />
+            </div>
+        </header>
     )
 }
 
